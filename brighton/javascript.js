3ecd478ec6641901b93912ff80bff18d1ba053a7
blissv <167956986+blissv@users.noreply.github.com>
2024-06-09 15:48:22 +0100

    Add files via upload

--- brighton/javascript.js
+++ brighton/javascript.js
@@ -1,10 +1,16 @@
 /* NAV BAR copied from W3Schools https://www.w3schools.com/howto/howto_js_topnav_responsive.asp */
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-  }  
+  }  */
+ 
+  /* W3Schools pop up code */
+  function myPop() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
