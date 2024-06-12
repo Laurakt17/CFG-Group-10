34cbf2f0918719d44701b704b93f737b38b673fc
blissv <167956986+blissv@users.noreply.github.com>
2024-06-12 11:18:15 +0100

    Update random-link.js
    duplicated random page function to allow for different paths from homepage nav

--- random-link.js
+++ random-link.js
@@ -13,3 +13,18 @@ function randomPage() {
         location.href = pages[i];
         console.log(location.href);
     }
+
+var pages2 = [
+    'belfast/belfast.html',
+    'birmingham/bham.html',
+    'glasgow/glasgow.html',
+    'london/london.html',
+    'manchester/manchester.html',
+    'brighton/brighton.html'
+    ];
+
+function randomPage2() {
+        var i = parseInt(Math.random()*pages2.length);
+        location.href = pages2[i];
+        console.log(location.href);
+    }
